23ece3e0df4e686a68b68b1324e4152d4c440949
Suraj Ambesange <surajambesange4474@gmail.com>
2025-12-05 19:46:42 +0530

    Modified Files

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,6 +1,13 @@
-let a= 2;
-let b= 5;
+// let a= 2;
+// let b= 5;
 
 // console.log(a*b);
 
-console.log(a+b);
+// console.log(a+b);
+// for (let i = 1; i<=5; i++){
+//     console.log(i);
+// }
+
+for (i=1; i<=5; i++){
+    console.log("Suraj")
+}
