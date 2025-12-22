5d947fb9b38bd3d9fbe23f7113d1fe3de82432b3
Suraj Ambesange <surajambesange4474@gmail.com>
2025-12-22 19:13:00 +0530

    Modified Files

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,24 +1,28 @@
-// let a= 2;
-// let b= 5;
+// // let a= 2;
+// // let b= 5;
 
-// console.log(a*b);
+// // console.log(a*b);
 
-// console.log(a+b);
-// for (let i = 1; i<=5; i++){
+// // console.log(a+b);
+// // for (let i = 1; i<=5; i++){
+// //     console.log(i);
+// // }
+
+// // for (i=1; i<=15; i++){
+// //     console.log("Suraj Ambesange")
+// // }
+
+// // let sentence="My name is Suraj Ambesange";
+// // let word=sentence.split(" ");
+// // console.log(word);
+
+// function myName(){
+//     for(let i=0; i<=100; i++){
 //     console.log(i);
+//     }
 // }
-
-// for (i=1; i<=15; i++){
-//     console.log("Suraj Ambesange")
-// }
-
-// let sentence="My name is Suraj Ambesange";
-// let word=sentence.split(" ");
-// console.log(word);
-
-function myName(){
-    for(let i=0; i<=100; i++){
-    console.log(i);
-    }
-}
-myName();    
+// myName();    
+let arr=[1,2,3];
+console.log(typeof(arr));
+arr.pop('Suraj');
+console.log(arr);
